065a916a6d98872b814ad03bfdcbe3444b2bc39a
victor-prm <victorpabb@gmail.com>
2025-01-28 10:13:09 +0100

    removed console logs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 let randomIndex = Math.floor(Math.random()*quotes.length);
 let blockQuoteElement = document.querySelector('#quote-of-the-day');
 let authorElement = document.querySelector('#quote-of-the-day-author');
-console.log(authorElement)
 
 blockQuoteElement.innerHTML = quotes[randomIndex].quote;
 authorElement.innerHTML = quotes[randomIndex].author;
-//console.log(randomIndex, quotes[randomIndex])
